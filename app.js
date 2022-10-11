cba81502e71fd96d4bf0e3f9df4ac071c664b23d
mynkbis <77193059+mynkbis@users.noreply.github.com>
2022-10-11 09:43:28 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,56 @@
+
+import './App.css';import React, { useState, useRef } from "react";
+import { useScript } from "./hooks";
+import jwt_deocde from "jwt-decode";
+
+
+const App = () => {
+  const googlebuttonref = useRef();
+  const [user, setuser] = useState(false);
+  const onGoogleSignIn = (user) => {
+    let userCred = user.credential;
+    let payload = jwt_deocde(userCred);
+    console.log(payload);
+    setuser(payload);
+  };
+  useScript("https://accounts.google.com/gsi/client", () => {
+    window.google.accounts.id.initialize({
+      client_id: "", // here's your Google ID
+      callback: onGoogleSignIn,
+      auto_select: false,
+    });
+
+    window.google.accounts.id.renderButton(googlebuttonref.current, {
+      size: "large",
+    });
+  });
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      {!user && <div ref={googlebuttonref}></div>}
+      {user && (
+        <div>
+          <h1>{user.name}</h1>
+          <img src={user.picture} alt="profile" />
+          <p>{user.email}</p>
+
+          <button
+            onClick={() => {
+              setuser(false);
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
